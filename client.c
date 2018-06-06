8ce3e6337cee7aaf7037d9d2472afc3a51c0f9c2
katiecai <katiecai@g.ucla.edu>
2018-06-05 21:30:03 -0700

    added ack field

--- client.c
+++ client.c
@@ -13,7 +13,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define SERVERPORT "4950"    // the port users will be connecting to
+#define SERVERPORT "4950" // the port users will be connecting to
 #define DATA 1008
 #define MAXBUFLEN 100
 #define CWND 5120
@@ -26,6 +26,7 @@ struct packet
   //2 = data
   //3 = retransmission
   int seq_num;
+  int ack_num;
 
   int data_size;
   //end of file, fin = 1
@@ -44,8 +45,9 @@ int main(int argc, char *argv[])
   int rv;
   int numbytes;
 
-  if (argc != 3) {
-    fprintf(stderr,"usage: client hostname message\n");
+  if (argc != 3)
+  {
+    fprintf(stderr, "usage: client hostname message\n");
     exit(1);
   }
 
@@ -53,15 +55,18 @@ int main(int argc, char *argv[])
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_DGRAM;
 
-  if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+  if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0)
+  {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
     return 1;
   }
 
   // loop through all the results and make a socket
-  for(p = servinfo; p != NULL; p = p->ai_next) {
+  for (p = servinfo; p != NULL; p = p->ai_next)
+  {
     if ((sockfd = socket(p->ai_family, p->ai_socktype,
-			 p->ai_protocol)) == -1) {
+                         p->ai_protocol)) == -1)
+    {
       perror("client: socket");
       continue;
     }
@@ -69,142 +74,130 @@ int main(int argc, char *argv[])
     break;
   }
 
-  if (p == NULL) {
+  if (p == NULL)
+  {
     fprintf(stderr, "client: failed to create socket\n");
     return 2;
   }
 
   int client_seq_num = 0;
 
-
-  while(1)
+  while (1)
+  {
+    struct packet rcv_packet;
+    //send initial three way handshake request
+    struct packet syn_packet;
+    syn_packet.type = 0;
+    syn_packet.seq_num = client_seq_num;
+    syn_packet.fin = 1;
+    if ((numbytes = sendto(sockfd, &syn_packet, sizeof(syn_packet), 0, p->ai_addr, p->ai_addrlen)) == -1)
     {
-      struct packet rcv_packet;
-      //send initial three way handshake request
-      struct packet syn_packet;
-      syn_packet.type = 0;
-      syn_packet.seq_num = client_seq_num;
-      syn_packet.fin = 1;      
-      if ((numbytes = sendto(sockfd, &syn_packet, sizeof(syn_packet), 0, p->ai_addr, p->ai_addrlen)) == -1) {
-	perror("client: sendto");
-	exit(1);
+      perror("client: sendto");
+      exit(1);
+    }
+    printf("Sending packet 0 SYN\n");
+    client_seq_num++;
+
+    if ((numbytes = recvfrom(sockfd, &rcv_packet, MAXBUFLEN - 1, 0,
+                             (struct sockaddr *)p->ai_addr, &(p->ai_addrlen))) == -1)
+    {
+      perror("recvfrom");
+      exit(1);
+    }
+
+    if (rcv_packet.type == 1 && rcv_packet.seq_num == 0)
+    {
+      printf("Receiving packet %d\n", rcv_packet.seq_num + 1);
+      struct packet ack_synack_packet;
+      ack_synack_packet.type = 0;
+      ack_synack_packet.seq_num = client_seq_num;
+      ack_synack_packet.fin = 1;
+      if ((numbytes = sendto(sockfd, &ack_synack_packet, sizeof(ack_synack_packet), 0, p->ai_addr, p->ai_addrlen)) == -1)
+      {
+        perror("server: sendto");
+        exit(1);
       }
-      printf("Sending packet 0 SYN\n");
+      printf("Sending packet %d SYN\n", ack_synack_packet.seq_num);
+      client_seq_num++;
+    }
+
+    // Send filename over
+    int fileNameBytes = 0;
+    if ((fileNameBytes = sendto(sockfd, argv[2], strlen(argv[2]) + 1, 0, p->ai_addr, p->ai_addrlen)) == -1)
+    {
+      perror("server: sendto");
+      exit(1);
+    }
+
+    // Receiving file
+    struct packet file_packet;
+    int fileBytes = 0;
+    if ((fileBytes = recvfrom(sockfd, &file_packet, MAXBUFLEN - 1, 0, (struct sockaddr *)p->ai_addr, &(p->ai_addrlen))) == -1)
+    {
+      perror("recvfrom");
+      exit(1);
+    }
+
+    if (file_packet.fin == 1)
+    {
+      file_packet.ack_num = file_packet.seq_num + file_packet.data_size + 16;
+      printf("Receiving packet %d\n", file_packet.ack_num);
+      //send the ACK
+    }
+    else //file_packet.fin == 0, there are more packets to receive
+    {
+      file_packet.ack_num = file_packet.seq_num + file_packet.data_size + 16;
+      printf("Receiving packet %d\n", file_packet.ack_num);
+
+      //send the ACK
+      struct packet firstACK_packet;
+      firstACK_packet.type = 0;
+      firstACK_packet.seq_num = client_seq_num;
+      firstACK_packet.fin = 1;
+      if ((numbytes = sendto(sockfd, &firstACK_packet, sizeof(firstACK_packet), 0, p->ai_addr, p->ai_addrlen)) == -1)
+      {
+        perror("server: sendto");
+        exit(1);
+      }
+      printf("Sending packet %d\n", firstACK_packet.seq_num);
       client_seq_num++;
 
-      if ((numbytes = recvfrom(sockfd, &rcv_packet, MAXBUFLEN-1 , 0,
-			       (struct sockaddr *)p->ai_addr, &(p->ai_addrlen))) == -1) {
-	perror("recvfrom");
-	exit(1);
+      //continually receive packets and send ACKs until file is complete
+      while (1)
+      {
+        struct packet large_file_packet;
+        int fileBytes = 0;
+        if ((fileBytes = recvfrom(sockfd, &large_file_packet, MAXBUFLEN - 1, 0, (struct sockaddr *)p->ai_addr, &(p->ai_addrlen))) == -1)
+        {
+          perror("recvfrom");
+          exit(1);
+        }
+
+        large_file_packet.ack_num = large_file_packet.seq_num + large_file_packet.data_size + 16;
+        printf("Receiving packet %d\n", large_file_packet.ack_num);
+
+        //send the ACK
+        struct packet ACK_packet;
+        ACK_packet.type = 0;
+        ACK_packet.seq_num = client_seq_num;
+        ACK_packet.fin = 1;
+        if ((numbytes = sendto(sockfd, &ACK_packet, sizeof(ACK_packet), 0, p->ai_addr, p->ai_addrlen)) == -1)
+        {
+          perror("server: sendto");
+          exit(1);
+        }
+        printf("Sending packet %d\n", ACK_packet.seq_num);
+        client_seq_num++;
+
+        if (large_file_packet.fin == 1)
+          break;
       }
-
-      if (rcv_packet.type == 1 && rcv_packet.seq_num == 0)
-	{
-	  printf("Receiving packet %d\n", rcv_packet.seq_num+1);
-	  struct packet ack_synack_packet;
-	  ack_synack_packet.type = 0;
-	  ack_synack_packet.seq_num = client_seq_num;
-	  ack_synack_packet.fin = 1;
-	  if ((numbytes = sendto(sockfd, &ack_synack_packet, sizeof(ack_synack_packet), 0, p->ai_addr, p->ai_addrlen)) == -1) {
-	    perror("server: sendto");
-	    exit(1);
-	  }
-	  printf("Sending packet %d SYN\n", ack_synack_packet.seq_num);
-	  client_seq_num++;
-	}
-
-      // Send filename over
-      int fileNameBytes = 0;
-      if ((fileNameBytes = sendto(sockfd, argv[2], strlen(argv[2])+1, 0, p->ai_addr, p->ai_addrlen)) == -1) {
-	perror("server: sendto");
-	exit(1);
-      }
-
-      // Receiving file
-      struct packet file_packet;
-      int fileBytes = 0;
-      if ((fileBytes = recvfrom(sockfd, &file_packet, MAXBUFLEN-1, 0, (struct sockaddr *)p->ai_addr, &(p->ai_addrlen))) == -1) {
-	perror("recvfrom");
-	exit(1);
-      }
-
-      if (file_packet.fin == 1)
-	{
-	  printf("Receiving packet %d\n", file_packet.seq_num + file_packet.data_size + 16);
-	  //send the ACK
-	}
-      else //file_packet.fin == 0, there are more packets to receive
-	{
-	  printf("Receiving packet %d\n", file_packet.seq_num + file_packet.data_size + 16);
-	  
-	  //send the ACK
-	  struct packet firstACK_packet;
-	  firstACK_packet.type = 0;
-	  firstACK_packet.seq_num = client_seq_num;
-	  firstACK_packet.fin = 1;
-	  if ((numbytes = sendto(sockfd, &firstACK_packet, sizeof(firstACK_packet), 0, p->ai_addr, p->ai_addrlen)) == -1) {
-	    perror("server: sendto");
-	    exit(1);
-	  }
-	  printf("Sending packet %d\n", firstACK_packet.seq_num);
-	  client_seq_num++;
-
-	  //continually receive packets and send ACKs until file is complete
-	  while(1)
-	    {	      	      
-	      struct packet large_file_packet;
-	      int fileBytes = 0;
-	      if ((fileBytes = recvfrom(sockfd, &large_file_packet, MAXBUFLEN-1, 0, (struct sockaddr *)p->ai_addr, &(p->ai_addrlen))) == -1) {
-		perror("recvfrom");
-		exit(1);
-	      }
-	      printf("Receiving packet %d\n", large_file_packet.seq_num + large_file_packet.data_size + 16);
-
-	      //send the ACK
-	      struct packet ACK_packet;
-	      ACK_packet.type = 0;
-	      ACK_packet.seq_num = client_seq_num;
-	      ACK_packet.fin = 1;
-	      if ((numbytes = sendto(sockfd, &ACK_packet, sizeof(ACK_packet), 0, p->ai_addr, p->ai_addrlen)) == -1) {
-		perror("server: sendto");
-		exit(1);
-	      }
-	      printf("Sending packet %d\n", ACK_packet.seq_num);
-	      client_seq_num++;
-
-	      if (large_file_packet.fin == 1)
-		break;	      
-	    }
-	}
-      
-      
-      break;
-
     }
-  /*
-  struct packet test_packet;
-  memset((void*) &test_packet, 0, sizeof(struct packet));
-  //  buffer = (struct packet*) malloc(sizeof(struct packet));
 
-  test_packet.type = 0;
-  test_packet.seq_num = 0;
-  
-  char* test = "TEST DATA";
-
-  memcpy((void*) &test_packet.data, (void*) test, strlen(test)+1);
-  test_packet.data_size = sizeof(test_packet.data);
-  test_packet.fin = 1;
-
-  if ((numbytes = sendto(sockfd, &test_packet, sizeof(test_packet), 0,
-			 p->ai_addr, p->ai_addrlen)) == -1) {
-    perror("client: sendto");
-    exit(1);
+    break;
   }
 
-  freeaddrinfo(servinfo);
-
-  printf("client: sent %d bytes to %s\n", numbytes, argv[1]);*/
   close(sockfd);
-  
+
   return 0;
 }
